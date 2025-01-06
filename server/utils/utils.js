bb88edd8f16a0894899c3450271e5a7fe3568f84
Heikki Riihinen <hekuttaja@gmail.com>
2025-01-06 17:22:37 +0200

    Remove unused utils from server

--- server/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const generateRandomId = () => {
-  const date = Date.now().toString();
-  const random = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
-  return `${date}-${random}`;
-}
